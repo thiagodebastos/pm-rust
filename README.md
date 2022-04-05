63692cbbc484ca0ca3b30ade52a38f1a1cffae75
Thiago de Bastos <6151341+thiagodebastos@users.noreply.github.com>
2022-04-05 22:55:06 +1000

    WIP: begin implementing core features
    As I work through the rust book, I add features.

---
+++ README.md
@@ -0,0 +1,44 @@
+## A terminal project manager written in Rust
+
+
+### Features:
+
+#### Core:
+
+To begin with, I want feature parity with [EivindArvesen's
+prm](https://github.com/EivindArvesen/prm)
+
+
+### Todo:
+
+- [ ]  List active project instances.
+- [ ]  Add project(s).
+- [ ]  Copy project.
+- [ ]  Edit project(s).
+- [ ]  List all projects.
+- [ ]  Remove project(s).
+- [ ]  Rename project.
+- [ ]  Start project.
+- [ ]  Stop active project.
+- [ ]  Display this information.
+- [ ]  Display version info.
+
+
+## Usage
+
+```shell
+usage: prm <option> [<args>] ...
+
+Options:
+  active                   List active project instances.
+  add <project name>       Add project(s).
+  copy <old> <new>         Copy project.
+  edit <project name>      Edit project(s).
+  list                     List all projects.
+  remove <project name>    Remove project(s).
+  rename <old> <new>       Rename project.
+  start <project name>     Start project.
+  stop                     Stop active project.
+  -h --help                Display this information.
+  -v --version             Display version info.
+```
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,123 @@
-use std::io;
+use std::{env, io};
 
-type Project = String;
-type Projects = Vec<String>;
+struct Project {
+    name: String,
+    path: String,
+}
+
+impl Project {
+    fn new(name: &str, path: &str) -> Project {
+        Project {
+            name: name.to_string(),
+            path: path.to_string(),
+        }
+    }
+}
+
+type Projects = Vec<Project>;
 
 fn main() {
-    let mut projects: Projects = Vec::new();
+    let args: Vec<String> = env::args().collect();
 
-    loop {
-        add_project(&mut projects);
-        select_project(&projects);
+    let mut projects: Projects = Vec::new();
+    for arg in args {
+        match arg.as_str() {
+            "add" => {
+                add_project(&mut projects);
+            }
+            "copy" => {
+                // copy_project(project);
+            }
+            "edit" => {
+                // edit_project(project);
+            }
+            "list" => {
+                list_all_projects(&projects);
+            }
+            "remove" => {
+                // remove_project(project, &mut projects);
+            }
+            "rename" => {
+                // rename_project(project);
+            }
+            "start" => {
+                // start_project(title, path);
+            }
+            "stop" => {
+                // stop_active_project(project);
+            }
+            "--version" | "-v" => display_version_info(),
+            "--help" | "-h" | _ => {
+                // display_help();
+            }
+        }
     }
+
+    // add_project(&mut projects);
 }
 
 fn add_project(projects: &mut Projects) {
-    let mut project: Project = String::new();
+    let mut project_name = String::new();
+    let mut project_path = String::new();
 
     println!("Enter a project name:");
-
     io::stdin()
-        .read_line(&mut project)
+        .read_line(&mut project_name)
         .expect("Failed to read line.");
 
-    projects.push(project);
+    println!("Enter a project path:");
+    io::stdin()
+        .read_line(&mut project_path)
+        .expect("Failed to read line.");
 
-    println!("Added new project: {}", projects[projects.len() - 1]);
+    let new_project = Project::new(&project_name, &project_path);
+
+    projects.push(new_project);
+
+    println!("Added new project: {}", project_name);
 }
 
-fn select_project(projects: &Projects) {
-    println!("Select a project from list:");
+// TODO add gettable desciption to each function
 
-    for project in projects {
-        println!("{}", project);
-    }
+fn list_active_projects(projects: &Projects) -> &Projects {
+    todo!();
+}
+
+fn copy_project(project: &Project) -> Project {
+    todo!();
+}
+
+fn edit_project(project: &mut Project) -> Project {
+    todo!();
+}
+
+fn list_all_projects(projects: &Projects) -> Projects {
+    todo!();
+}
+
+fn remove_project(project: Project, projects: &mut Projects) -> Projects {
+    todo!();
+}
+
+fn rename_project(project: &mut Project) -> Project {
+    todo!();
+}
+
+fn start_project(title: String, path: String) -> Project {
+    todo!();
+}
+
+fn stop_active_project(project: Project) -> Project {
+    todo!();
+}
+
+fn display_help() -> Vec<String> {
+    // display each function's description
+    todo!();
+}
+
+fn display_version_info() {
+    let version = env!("CARGO_PKG_VERSION");
+    println!("pm version {}", version);
+    println!("Written by Thiago de Bastos, 2022");
 }
